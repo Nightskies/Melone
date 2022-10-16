9b53f78f58769d75647a3211057d7d673798df5e
Nightskies <48439174+Nightskies@users.noreply.github.com>
2022-10-16 16:29:04 +0700

    Added libs

---
+++ Melone/Vendor/yaml-cpp/premake5.lua
@@ -0,0 +1,39 @@
+project "yaml-cpp"
+	architecture "x86_64"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin_int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
